812f72beb4d8dbdd6c370bf54d586c9a403b83a2
UemKaLaunda <dasanishka157@gmail.com>
2026-03-27 09:25:25 +0530

    Configured Database Connection

--- social-media-backend/src/main/java/com/socialmedia/social_media_backend/model/Comment.java
+++ social-media-backend/src/main/java/com/socialmedia/social_media_backend/model/Comment.java
@@ -1,8 +1,6 @@
 package com.socialmedia.social_media_backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.socialmedia.social_media_backend.model.Post;
-import com.socialmedia.social_media_backend.model.User;
 import jakarta.persistence.*;
 import lombok.*;
 
