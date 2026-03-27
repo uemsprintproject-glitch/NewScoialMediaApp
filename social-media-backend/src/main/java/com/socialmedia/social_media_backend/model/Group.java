732c5e926d4caebef261b41e8192dde73c0321a8
Anshi016 <anship308@gmail.com>
2026-03-27 14:34:35 +0530

    edited group

---
+++ social-media-backend/src/main/java/com/socialmedia/social_media_backend/model/Group.java
@@ -0,0 +1,24 @@
+package com.socialmedia.social_media_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "`Groups`") 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer groupID;
+
+    @Column(name = "groupName", nullable = false, unique = true)
+    private String groupName;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "adminID", nullable = false)
+    private User admin;
+}
